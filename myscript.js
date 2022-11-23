f418186ad5f1eb4b4d4c1748c2909959097f7e26
Adesh Gawade <117651499+Adeshc2k@users.noreply.github.com>
2022-11-23 18:16:34 +0530

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,13 @@
+//  let play =document.querySelectorAll(#song-play);
+
+//  play.addEventListener("click" function(){
+
+//  });
+
+
+var mySong = new Audio();
+mysong.src = "songOfEmaran/128-Baarish - Yaariyan - Yaariyan 128 Kbps.mp3";
+
+const playSong = () => {
+    mySong.play();
+};
